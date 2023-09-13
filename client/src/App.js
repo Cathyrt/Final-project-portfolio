c83b671966f9d3fa6eccff9266c734f50b338eab
Cathyrt <110995939+Cathyrt@users.noreply.github.com>
2023-09-13 18:49:42 +0200

    Signin file

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import SignIn from './components/pages/SignIn';
 import Destination from './components/pages/Destination';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path='/services' element={<Services />} />
         <Route path='/products' element={<Products />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<SignIn />} />
         <Route path='/services/activity' element={<Destination />} />
 
       </Routes>
